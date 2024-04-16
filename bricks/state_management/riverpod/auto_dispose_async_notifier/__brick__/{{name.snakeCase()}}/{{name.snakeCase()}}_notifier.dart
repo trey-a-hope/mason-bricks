0b05d42dc7a60cbb0a1bfc56d3c0a25b88431718
Trey Hope <99027548+trey-a-hope@users.noreply.github.com>
2024-04-16 00:38:25 -0400

    New Bricks
    * Added mason bricks.

---
+++ bricks/state_management/riverpod/auto_dispose_async_notifier/__brick__/{{name.snakeCase()}}/{{name.snakeCase()}}_notifier.dart
@@ -0,0 +1,7 @@
+import 'dart:async';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class {{name.pascalCase()}}Notifier extends AutoDisposeAsyncNotifier<{{type}}?> {
+  @override
+  FutureOr<{{type}}?> build() => null;
+}
