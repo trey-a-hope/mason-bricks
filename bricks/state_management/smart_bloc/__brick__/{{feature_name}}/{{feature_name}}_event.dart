02a83ecefcfd547aa09cdfc4ed46770daaa62779
Trey Hope <99027548+trey-a-hope@users.noreply.github.com>
2025-02-10 23:46:04 -0500

    wip
    wip

--- bricks/state_management/smart_bloc/__brick__/{{feature_name}}/{{feature_name}}_event.dart
+++ bricks/state_management/smart_bloc/__brick__/{{feature_name}}/{{feature_name}}_event.dart
@@ -8,6 +8,34 @@ class Fetch{{feature_name.pascalCase()}} extends {{feature_name.pascalCase()}}Ev
 class FetchMore{{feature_name.pascalCase()}} extends {{feature_name.pascalCase()}}Event {}
 {{/include_fetch}}
 
+{{#include_create}}
+class Create{{feature_name.pascalCase()}} extends {{feature_name.pascalCase()}}Event {
+  final Map<String, dynamic> data;
+
+  Create{{feature_name.pascalCase()}}({required this.data});
+}
+{{/include_create}}
+
+{{#include_read}}
+class Read{{feature_name.pascalCase()}} extends {{feature_name.pascalCase()}}Event {
+  final String id;
+
+  Read{{feature_name.pascalCase()}}({required this.id});
+}
+{{/include_read}}
+
+{{#include_update}}
+class Update{{feature_name.pascalCase()}} extends {{feature_name.pascalCase()}}Event {
+  final String id;
+  final Map<String, dynamic> data;
+
+  Update{{feature_name.pascalCase()}}({
+    required this.id,
+    required this.data,
+  });
+}
+{{/include_update}}
+
 {{#include_delete}}
 class Delete{{feature_name.pascalCase()}} extends {{feature_name.pascalCase()}}Event {
   final String id;
