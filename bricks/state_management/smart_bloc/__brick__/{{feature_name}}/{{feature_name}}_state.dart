2fc33c3279cb16c33f490cf28f4e9108aa00bf74
Trey Hope <99027548+trey-a-hope@users.noreply.github.com>
2025-02-11 00:19:30 -0500

    wip
    wip

--- bricks/state_management/smart_bloc/__brick__/{{feature_name}}/{{feature_name}}_state.dart
+++ bricks/state_management/smart_bloc/__brick__/{{feature_name}}/{{feature_name}}_state.dart
@@ -2,33 +2,21 @@ part of '{{feature_name.snakeCase()}}_bloc.dart';
 
 class {{feature_name.pascalCase()}}sState {
   final String? cursor;
-  {{#base_state_vars}}
-  final {{{.}}}
-  {{/base_state_vars}}
 
   {{feature_name.pascalCase()}}sState({
     required this.cursor,
-    {{#base_state_vars}}
-    required this.{{{.}}},
-    {{/base_state_vars}}
   });
 }
 
 class {{feature_name.pascalCase()}}sInitial extends {{feature_name.pascalCase()}}sState {
   {{feature_name.pascalCase()}}sInitial({
     required super.cursor,
-    {{#base_state_vars}}
-    required super.{{{.}}},
-    {{/base_state_vars}}
   });
 }
 
 class {{feature_name.pascalCase()}}sLoading extends {{feature_name.pascalCase()}}sState {
   {{feature_name.pascalCase()}}sLoading({
     required super.cursor,
-    {{#base_state_vars}}
-    required super.{{{.}}},
-    {{/base_state_vars}}
   });
 }
 
@@ -36,11 +24,8 @@ class {{feature_name.pascalCase()}}sLoaded extends {{feature_name.pascalCase()}}
   final List<{{model_name.pascalCase()}}> {{feature_name.camelCase()}}s;
 
   {{feature_name.pascalCase()}}sLoaded({
-    required this.{{feature_name.camelCase()}},
+    required this.{{feature_name.camelCase()}}s,
     required super.cursor,
-    {{#base_state_vars}}
-    required super.{{{.}}},
-    {{/base_state_vars}}
   });
 }
 
@@ -50,9 +35,6 @@ class {{feature_name.pascalCase()}}sSuccess extends {{feature_name.pascalCase()}
   {{feature_name.pascalCase()}}sSuccess({
     required this.message,
     required super.cursor,
-    {{#base_state_vars}}
-    required super.{{{.}}},
-    {{/base_state_vars}}
   });
 }
 
@@ -62,8 +44,5 @@ class {{feature_name.pascalCase()}}sError extends {{feature_name.pascalCase()}}s
   {{feature_name.pascalCase()}}sError({
     required this.message,
     required super.cursor,
-    {{#base_state_vars}}
-    required super.{{{.}}},
-    {{/base_state_vars}}
   });
 }
