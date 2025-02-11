c3d9b5255e605129535feca11890399345615eea
Trey Hope <99027548+trey-a-hope@users.noreply.github.com>
2025-02-10 23:50:26 -0500

    wip
    wip

--- bricks/state_management/smart_bloc/__brick__/{{feature_name}}/{{feature_name}}_bloc.dart
+++ bricks/state_management/smart_bloc/__brick__/{{feature_name}}/{{feature_name}}_bloc.dart
@@ -9,8 +9,11 @@ class {{feature_name.pascalCase()}}Bloc extends Bloc<{{feature_name.pascalCase()
     required this.authBloc,
   })  : _nakamaService = NakamaService(),
         super({{feature_name.pascalCase()}}Initial(cursor: null)) {
-   {{#include_fetch}}on<Fetch{{feature_name.pascalCase()}}>(_onFetch{{feature_name.pascalCase()}});
+    {{#include_fetch}}on<Fetch{{feature_name.pascalCase()}}>(_onFetch{{feature_name.pascalCase()}});
     on<FetchMore{{feature_name.pascalCase()}}>(_onFetchMore{{feature_name.pascalCase()}});{{/include_fetch}}
+    {{#include_create}}on<Create{{feature_name.pascalCase()}}>(_onCreate{{feature_name.pascalCase()}});{{/include_create}}
+    {{#include_read}}on<Read{{feature_name.pascalCase()}}>(_onRead{{feature_name.pascalCase()}});{{/include_read}}
+    {{#include_update}}on<Update{{feature_name.pascalCase()}}>(_onUpdate{{feature_name.pascalCase()}});{{/include_update}}
     {{#include_delete}}on<Delete{{feature_name.pascalCase()}}>(_onDelete{{feature_name.pascalCase()}});{{/include_delete}}
   }
 
@@ -88,6 +91,111 @@ class {{feature_name.pascalCase()}}Bloc extends Bloc<{{feature_name.pascalCase()
     }
   }
   {{/include_fetch}}
+
+  {{#include_create}}
+  Future<void> _onCreate{{feature_name.pascalCase()}}(
+    Create{{feature_name.pascalCase()}} event,
+    Emitter<{{feature_name.pascalCase()}}State> emit,
+  ) async {
+    emit({{feature_name.pascalCase()}}Loading(cursor: state.cursor));
+
+    try {
+      // TODO: Implement create logic here
+      // await _nakamaService.create{{feature_name.pascalCase()}}(event.data);
+
+      emit(
+        {{feature_name.pascalCase()}}Success(
+          message: '{{feature_name.pascalCase()}} created successfully',
+          cursor: state.cursor,
+        ),
+      );
+    } on GrpcError catch (e) {
+      emit(
+        {{feature_name.pascalCase()}}Error(
+          message: e.message ?? 'Unknown GRPC Error: ${e.codeName}',
+          cursor: state.cursor,
+        ),
+      );
+    } catch (e) {
+      emit(
+        {{feature_name.pascalCase()}}Error(
+          message: 'Unexpected error: ${e.toString()}',
+          cursor: state.cursor,
+        ),
+      );
+    }
+  }
+  {{/include_create}}
+
+  {{#include_read}}
+  Future<void> _onRead{{feature_name.pascalCase()}}(
+    Read{{feature_name.pascalCase()}} event,
+    Emitter<{{feature_name.pascalCase()}}State> emit,
+  ) async {
+    emit({{feature_name.pascalCase()}}Loading(cursor: state.cursor));
+
+    try {
+      // TODO: Implement read logic here
+      // final {{feature_name.camelCase()}} = await _nakamaService.read{{feature_name.pascalCase()}}(event.id);
+
+      emit(
+        {{feature_name.pascalCase()}}Loaded(
+          {{feature_name.camelCase()}}: {{feature_name.camelCase()}},
+          cursor: state.cursor,
+        ),
+      );
+    } on GrpcError catch (e) {
+      emit(
+        {{feature_name.pascalCase()}}Error(
+          message: e.message ?? 'Unknown GRPC Error: ${e.codeName}',
+          cursor: state.cursor,
+        ),
+      );
+    } catch (e) {
+      emit(
+        {{feature_name.pascalCase()}}Error(
+          message: 'Unexpected error: ${e.toString()}',
+          cursor: state.cursor,
+        ),
+      );
+    }
+  }
+  {{/include_read}}
+
+  {{#include_update}}
+  Future<void> _onUpdate{{feature_name.pascalCase()}}(
+    Update{{feature_name.pascalCase()}} event,
+    Emitter<{{feature_name.pascalCase()}}State> emit,
+  ) async {
+    emit({{feature_name.pascalCase()}}Loading(cursor: state.cursor));
+
+    try {
+      // TODO: Implement update logic here
+      // await _nakamaService.update{{feature_name.pascalCase()}}(event.id, event.data);
+
+      emit(
+        {{feature_name.pascalCase()}}Success(
+          message: '{{feature_name.pascalCase()}} updated successfully',
+          cursor: state.cursor,
+        ),
+      );
+    } on GrpcError catch (e) {
+      emit(
+        {{feature_name.pascalCase()}}Error(
+          message: e.message ?? 'Unknown GRPC Error: ${e.codeName}',
+          cursor: state.cursor,
+        ),
+      );
+    } catch (e) {
+      emit(
+        {{feature_name.pascalCase()}}Error(
+          message: 'Unexpected error: ${e.toString()}',
+          cursor: state.cursor,
+        ),
+      );
+    }
+  }
+  {{/include_update}}
 
   {{#include_delete}}
   Future<void> _onDelete{{feature_name.pascalCase()}}(
