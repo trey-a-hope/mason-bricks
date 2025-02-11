e539b94ad04669cf8b7a9169d09db8413a5f6806
Trey Hope <99027548+trey-a-hope@users.noreply.github.com>
2025-02-11 00:32:45 -0500

    wip
    wip

--- bricks/state_management/smart_bloc/__brick__/{{feature_name}}/{{feature_name}}_state.dart
+++ bricks/state_management/smart_bloc/__brick__/{{feature_name}}/{{feature_name}}_state.dart
@@ -1,26 +1,26 @@
 part of '{{feature_name.snakeCase()}}_bloc.dart';
 
-class {{feature_name.pascalCase()}}sState {
+class {{feature_name.pascalCase()}}State {
   final String? cursor;
 
-  {{feature_name.pascalCase()}}sState({
+  {{feature_name.pascalCase()}}State({
     required this.cursor,
   });
 }
 
-class {{feature_name.pascalCase()}}sInitial extends {{feature_name.pascalCase()}}sState {
+class {{feature_name.pascalCase()}}sInitial extends {{feature_name.pascalCase()}}State {
   {{feature_name.pascalCase()}}sInitial({
     required super.cursor,
   });
 }
 
-class {{feature_name.pascalCase()}}sLoading extends {{feature_name.pascalCase()}}sState {
+class {{feature_name.pascalCase()}}sLoading extends {{feature_name.pascalCase()}}State {
   {{feature_name.pascalCase()}}sLoading({
     required super.cursor,
   });
 }
 
-class {{feature_name.pascalCase()}}sLoaded extends {{feature_name.pascalCase()}}sState {
+class {{feature_name.pascalCase()}}sLoaded extends {{feature_name.pascalCase()}}State {
   final List<{{feature_name.pascalCase()}}> {{feature_name.camelCase()}}s;
 
   {{feature_name.pascalCase()}}sLoaded({
@@ -29,7 +29,7 @@ class {{feature_name.pascalCase()}}sLoaded extends {{feature_name.pascalCase()}}
   });
 }
 
-class {{feature_name.pascalCase()}}sSuccess extends {{feature_name.pascalCase()}}sState {
+class {{feature_name.pascalCase()}}sSuccess extends {{feature_name.pascalCase()}}State {
   final String message;
 
   {{feature_name.pascalCase()}}sSuccess({
@@ -38,7 +38,7 @@ class {{feature_name.pascalCase()}}sSuccess extends {{feature_name.pascalCase()}
   });
 }
 
-class {{feature_name.pascalCase()}}sError extends {{feature_name.pascalCase()}}sState {
+class {{feature_name.pascalCase()}}sError extends {{feature_name.pascalCase()}}State {
   final String message;
 
   {{feature_name.pascalCase()}}sError({
