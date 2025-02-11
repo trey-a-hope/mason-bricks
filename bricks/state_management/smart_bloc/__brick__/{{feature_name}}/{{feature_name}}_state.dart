e8959b8f7e086c26f4c7e7724c9fe8b15b573fb8
Trey Hope <99027548+trey-a-hope@users.noreply.github.com>
2025-02-11 00:17:51 -0500

    wip
    wip

--- bricks/state_management/smart_bloc/__brick__/{{feature_name}}/{{feature_name}}_state.dart
+++ bricks/state_management/smart_bloc/__brick__/{{feature_name}}/{{feature_name}}_state.dart
@@ -33,7 +33,7 @@ class {{feature_name.pascalCase()}}sLoading extends {{feature_name.pascalCase()}
 }
 
 class {{feature_name.pascalCase()}}sLoaded extends {{feature_name.pascalCase()}}sState {
-  final List<{{model_name.pascalCase()}}> {{feature_name.camelCase()}};
+  final List<{{model_name.pascalCase()}}> {{feature_name.camelCase()}}s;
 
   {{feature_name.pascalCase()}}sLoaded({
     required this.{{feature_name.camelCase()}},
