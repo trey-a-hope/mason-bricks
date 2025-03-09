37068305087352867889327091f5c13ef16c21a8
Trey Hope <99027548+trey-a-hope@users.noreply.github.com>
2025-03-09 04:12:32 -0500

    wip
    wip

--- bricks/state_management/smart_bloc/__brick__/{{feature_name}}{{#include_fetch}}s{{\include_fetch}}/{{feature_name}}{{#include_fetch}}s{{\include_fetch}}_state.dart
+++ bricks/state_management/smart_bloc/__brick__/{{feature_name}}{{#include_fetch}}s{{\include_fetch}}/{{feature_name}}{{#include_fetch}}s{{\include_fetch}}_state.dart
@@ -3,23 +3,27 @@ part of '{{feature_name.snakeCase()}}_bloc.dart';
 sealed class {{feature_name.pascalCase()}}State {
   final {{state_object_type}}? {{feature_name.camelCase()}};
 
-  {{feature_name.pascalCase()}}State({this.{{feature_name.camelCase()}}});
+  {{feature_name.pascalCase()}}State(this.{{feature_name.camelCase()}});
 }
 
-class {{feature_name.pascalCase()}}Initial extends {{feature_name.pascalCase()}}State {}
+class {{feature_name.pascalCase()}}Initial extends {{feature_name.pascalCase()}}State {
+  {{feature_name.pascalCase()}}Initial(super.{{feature_name.camelCase()}});
+}
 
-class {{feature_name.pascalCase()}}Loading extends {{feature_name.pascalCase()}}State {}
+class {{feature_name.pascalCase()}}Loading extends {{feature_name.pascalCase()}}State {
+  {{feature_name.pascalCase()}}Loading(super.{{feature_name.camelCase()}});
+}
 
 class {{feature_name.pascalCase()}}Loaded extends {{feature_name.pascalCase()}}State {
-  {{feature_name.pascalCase()}}Loaded({super.{{feature_name.camelCase()}}});
+  {{feature_name.pascalCase()}}Loaded(super.{{feature_name.camelCase()}});
 }
 
 class {{feature_name.pascalCase()}}Success extends {{feature_name.pascalCase()}}State {
   final String message;
-  {{feature_name.pascalCase()}}Success(this.message);
+  {{feature_name.pascalCase()}}Success(super.{{feature_name.camelCase()}}, this.message);
 }
 
 class {{feature_name.pascalCase()}}Error extends {{feature_name.pascalCase()}}State {
   final String message;
-  {{feature_name.pascalCase()}}Error(this.message);
+  {{feature_name.pascalCase()}}Error(super.{{feature_name.camelCase()}}, this.message);
 }
